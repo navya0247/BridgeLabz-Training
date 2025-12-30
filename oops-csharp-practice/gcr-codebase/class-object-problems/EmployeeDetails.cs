d21cfbb15409d8d3d8e824ca7b779688b67d48ea
navya <navyay301@gmail.com>
2025-12-30 15:20:22 +0530

    initial commit

---
+++ oops-csharp-practice/gcr-codebase/class-object-problems/EmployeeDetails.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.oops.classAndObject
+{
+    internal class EmployeeDetails
+    {
+        // data
+        public string name;
+        public int id;
+        public double salary;
+
+        // method to display details
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Employee Name: " + name);
+            Console.WriteLine("Employee ID: " + id);
+            Console.WriteLine("Employee Salary: " + salary);
+        }
+
+        public static void Main(string[] args)
+        {
+            // object creation
+            EmployeeDetails emp = new EmployeeDetails();
+
+            // taking input from user
+            Console.WriteLine("Enter employee name ");
+            emp.name = Console.ReadLine();
+
+            Console.WriteLine("Enter employee id ");
+            emp.id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter employee salary ");
+            emp.salary = double.Parse(Console.ReadLine());
+
+            // method call
+            emp.DisplayDetails();
+        }
+    }
+
+}
+
---
+++ oops-csharp-practice/gcr-codebase/class-object-problems/HandleBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.oops.classAndObject
+{
+    internal class HandleBook
+    {
+        // book details
+        public string title;
+        public string author;
+        public double price;
+
+        // method to show details
+        public void DisplayBook()
+        {
+            Console.WriteLine("Book Title: " + title);
+            Console.WriteLine("Book Author: " + author);
+            Console.WriteLine("Book Price: " + price);
+        }
+
+        public static void Main(string[] args)
+        {
+            HandleBook b = new HandleBook();
+
+            // user input
+            Console.WriteLine("Enter book title:");
+            b.title = Console.ReadLine();
+
+            Console.WriteLine("Enter book author:");
+            b.author = Console.ReadLine();
+
+            Console.WriteLine("Enter book price:");
+            b.price = double.Parse(Console.ReadLine());
+
+            // calling method
+            b.DisplayBook();
+        }
+    }
+
+}
+
