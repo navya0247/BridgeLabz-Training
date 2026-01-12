12850e3063471450b94c74a937a7b4ea0b77026a
navya <navyay301@gmail.com>
2026-01-12 16:28:50 +0530

    initial commit

---
+++ oops-csharp-practice/scenario-based/customFurnitureManufacturing/WoodRod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.oops.scenarioBased.customFurnitureManufacturing
+{
+    internal class WoodRod
+    {
+            public int TotalLength;
+            public int[] Prices;
+
+            public WoodRod(int length)
+            {
+                TotalLength = length;
+                Prices = new int[length + 1];
+            }
+        }
+
+    }
+
