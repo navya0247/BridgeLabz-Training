fa4bf82913484c5a6c1505074312a02cf59f5017
navya <navyay301@gmail.com>
2026-01-13 14:36:13 +0530

    initial commit

---
+++ oops-csharp-practice/scenario-based/movieScheduleManager/MovieMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.oops.scenarioBased.movieScheduleManager
+{
+    internal class MovieMain
+    {
+        public static void Main(string[] args)
+        {
+            MovieUtility utility = new MovieUtility();
+
+            MovieMenu.ShowMenu(utility);
+        }
+    }
+}
+
+              
+
+
+
+
---
+++ oops-csharp-practice/scenario-based/movieScheduleManager/MovieMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BridgeLabzTraining.oops.scenarioBased.movieScheduleManager
+{
+    internal class MovieMenu
+    {
+        public static void ShowMenu(MovieUtility utility)
+        {
+            while (true)  
+            {
+                Console.WriteLine("\nCinemaTime Menu");
+                Console.WriteLine("1. Add Movie");
+                Console.WriteLine("2. Search Movie");
+                Console.WriteLine("3. Display All Movies");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter choice: ");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        utility.AddMovie();
+                        break;
+
+                    case 2:
+                        Console.Write("Enter movie name to search: ");
+                        utility.SearchMovie();
+                        break;
+
+                    case 3:
+                        utility.DisplayMovie();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Exiting CinemaTime...");
+                        return;  
+
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+        }
+    }
+}
