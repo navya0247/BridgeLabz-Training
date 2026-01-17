38e94819b9a7ed2b601cc30eca647659698c89b7
navya <navyay301@gmail.com>
2026-01-17 14:37:34 +0530

    fitness tracker

---
+++ dsa-csharp-practice/scenario-based/fitness-tracker/FitnessMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.dataStructures.scenarioBased.fitnessTracker
+{
+    internal class FitnessMain
+    {
+        public static void Main(string[] args)
+        {
+            FitnessMenu menu = new FitnessMenu();
+            menu.DisplayMenu();
+
+        }
+        
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/fitness-tracker/IFitness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.dataStructures.scenarioBased.fitnessTracker
+{
+        internal interface IFitness
+        {
+            void AddUser();          // Add new user
+            void ShowSteps();        // Show steps of all users
+            void ShowLeaderBoard();  // Show sorted leaderboard
+        }
+    }
+
+
