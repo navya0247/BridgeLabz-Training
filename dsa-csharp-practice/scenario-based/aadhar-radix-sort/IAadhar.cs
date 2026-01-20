2cd5882bc520fa8624130b22cdc1a8a35f7871ec
navya <navyay301@gmail.com>
2026-01-20 12:39:32 +0530

    aadhar radix sort

---
+++ dsa-csharp-practice/scenario-based/aadhar-radix-sort/IAadhar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.dataStructures.scenarioBased.AadharNumber
+{
+    // Interface defining methods for Aadhaar number operations
+    internal interface IAadhar
+    {
+        void Input();         // Method to take Aadhaar numbers input from user
+        void Display();       // Method to display all Aadhaar numbers
+        void RadixSort();     // Method to sort Aadhaar numbers using Radix Sort
+        void BinarySearch();  // Method to search a number using Binary Search
+    }
+}
