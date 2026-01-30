bd1876d6c4e863966b10e4d43953f3e094ecb487
navya <navyay301@gmail.com>
2026-01-30 16:21:36 +0530

    [Navya] added ambulance route

---
+++ collections-csharp-practice/scenario-based/ambulance-route/HospitalMain.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+    internal class HospitalMain
+    {
+        // Entry point of the application
+        static void Main()
+        {
+            // Create menu object
+            HospitalMenu menu = new HospitalMenu();
+
+            // Display hospital menu
+            menu.ShowMenu();
+        }
+    }
